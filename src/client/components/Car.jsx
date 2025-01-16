ad78a27a4de412f0495c571398b5b867d291f476
mujahidina <mujahidabdi5070@gmail.com>
2025-01-16 17:16:14 +0300

    seeded the database with images

--- src/client/components/Car.jsx
+++ src/client/components/Car.jsx
@@ -3,7 +3,7 @@ function Car({name, price, image_url}){
 
         <div className="">
            <div className="w-96 h-96 mb-8 mr-5 border border-slate-400 rounded-md">
-                        <img src={image_url} className="h-[60%] w-full object-cover" alt="car"/>
+                        <img src={image_url} className="h-[60%] w-[90%] object-cover mx-auto" alt="car"/>
                         <span className=''>{name}</span>
                         <h1>{price}</h1>
                         <button className="bg-black text-white py-2 px-4"  >BOOK NOW</button>
--- src/client/components/Dashboard.jsx
+++ src/client/components/Dashboard.jsx
@@ -6,7 +6,7 @@ function Dashboard(){
         <div className="h-[100vh] w-[85%] right-0 absolute">
             {/* top */}
             <div className=" h-[8vh] w-full flex flex-row justify-between bg-white border-b border-slate-400  pt-4">
-               <h1 className="ml-8">Wheels4Rent</h1>
+               <h1 className="ml-16">Wheels4Rent</h1>
                <h1 className="mr-16">Mujahid</h1>
             </div>
 
